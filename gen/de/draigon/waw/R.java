cdfe4a7cdb3ff788fa3ad4d69ebbf6bf11fe5b12
Onaan <onaan@onaan.com>
2012-06-09 14:55:22 +0200

    private-league: now with icons

--- gen/de/draigon/waw/R.java
+++ gen/de/draigon/waw/R.java
@@ -11,75 +11,79 @@ public final class R {
     public static final class attr {
     }
 
+    public static final class drawable {
+        public static final int icon = 0x7f020000;
+    }
+
     public static final class id {
-        public static final int b_matchdetails_bet_save = 0x7f050009;
-        public static final int b_set_login_data_save = 0x7f050011;
-        public static final int b_set_server_data_save = 0x7f050014;
-        public static final int b_startActivity_login = 0x7f050018;
-        public static final int b_startActivity_playing_schedule = 0x7f050015;
-        public static final int b_startActivity_ranking = 0x7f050017;
-        public static final int b_startActivity_special_bet = 0x7f050016;
-        public static final int cb_set_login_data_show = 0x7f050010;
-        public static final int et_matchdetails_bet_guest = 0x7f050007;
-        public static final int et_matchdetails_bet_home = 0x7f050006;
-        public static final int et_set_login_data_name = 0x7f05000b;
-        public static final int et_set_login_data_password = 0x7f05000c;
-        public static final int et_set_server_data_get_server = 0x7f050012;
-        public static final int et_set_server_data_post_server = 0x7f050013;
-        public static final int lv_all_match_bets = 0x7f050004;
-        public static final int lv_ranking = 0x7f05000a;
-        public static final int lv_teambet = 0x7f050019;
-        public static final int rb_set_login_data_server_select_1 = 0x7f05000e;
-        public static final int rb_set_login_data_server_select_2 = 0x7f05000f;
-        public static final int rg_set_login_data_server_select = 0x7f05000d;
-        public static final int server_location_menu_edit_server_data = 0x7f05001a;
-        public static final int t_all_match_bets_guest = 0x7f050003;
-        public static final int t_all_match_bets_guest_score = 0x7f050002;
-        public static final int t_all_match_bets_home = 0x7f050000;
-        public static final int t_all_match_bets_home_score = 0x7f050001;
-        public static final int t_matchdetails_guest = 0x7f050008;
-        public static final int t_matchdetails_home = 0x7f050005;
+        public static final int b_matchdetails_bet_save = 0x7f060009;
+        public static final int b_set_login_data_save = 0x7f060011;
+        public static final int b_set_server_data_save = 0x7f060014;
+        public static final int b_startActivity_login = 0x7f060018;
+        public static final int b_startActivity_playing_schedule = 0x7f060015;
+        public static final int b_startActivity_ranking = 0x7f060017;
+        public static final int b_startActivity_special_bet = 0x7f060016;
+        public static final int cb_set_login_data_show = 0x7f060010;
+        public static final int et_matchdetails_bet_guest = 0x7f060007;
+        public static final int et_matchdetails_bet_home = 0x7f060006;
+        public static final int et_set_login_data_name = 0x7f06000b;
+        public static final int et_set_login_data_password = 0x7f06000c;
+        public static final int et_set_server_data_get_server = 0x7f060012;
+        public static final int et_set_server_data_post_server = 0x7f060013;
+        public static final int lv_all_match_bets = 0x7f060004;
+        public static final int lv_ranking = 0x7f06000a;
+        public static final int lv_teambet = 0x7f060019;
+        public static final int rb_set_login_data_server_select_1 = 0x7f06000e;
+        public static final int rb_set_login_data_server_select_2 = 0x7f06000f;
+        public static final int rg_set_login_data_server_select = 0x7f06000d;
+        public static final int server_location_menu_edit_server_data = 0x7f06001a;
+        public static final int t_all_match_bets_guest = 0x7f060003;
+        public static final int t_all_match_bets_guest_score = 0x7f060002;
+        public static final int t_all_match_bets_home = 0x7f060000;
+        public static final int t_all_match_bets_home_score = 0x7f060001;
+        public static final int t_matchdetails_guest = 0x7f060008;
+        public static final int t_matchdetails_home = 0x7f060005;
     }
 
     public static final class layout {
-        public static final int all_match_bets = 0x7f020000;
-        public static final int bet_match = 0x7f020001;
-        public static final int ranking = 0x7f020002;
-        public static final int set_login_data = 0x7f020003;
-        public static final int set_server_data = 0x7f020004;
-        public static final int start_activity = 0x7f020005;
-        public static final int teambet = 0x7f020006;
+        public static final int all_match_bets = 0x7f030000;
+        public static final int bet_match = 0x7f030001;
+        public static final int ranking = 0x7f030002;
+        public static final int set_login_data = 0x7f030003;
+        public static final int set_server_data = 0x7f030004;
+        public static final int start_activity = 0x7f030005;
+        public static final int teambet = 0x7f030006;
     }
 
     public static final class menu {
-        public static final int server_location = 0x7f040000;
+        public static final int server_location = 0x7f050000;
     }
 
     public static final class string {
-        public static final int app_name = 0x7f030000;
-        public static final int default_get_server = 0x7f03000f;
-        public static final int default_post_server = 0x7f030010;
-        public static final int matchdetails_upload_bet = 0x7f030011;
-        public static final int rb_set_login_data_server_select_1 = 0x7f030013;
-        public static final int rb_set_login_data_server_select_2 = 0x7f030014;
-        public static final int server_location_menu_edit_server_data = 0x7f03000b;
-        public static final int set_login_data_choose_group = 0x7f030012;
-        public static final int set_login_data_name = 0x7f030005;
-        public static final int set_login_data_password = 0x7f030006;
-        public static final int set_login_data_save = 0x7f030008;
-        public static final int set_login_data_save_message = 0x7f030009;
-        public static final int set_login_data_show = 0x7f030007;
-        public static final int set_server_data_get_server = 0x7f03000c;
-        public static final int set_server_data_post_server = 0x7f03000d;
-        public static final int set_server_data_save = 0x7f03000e;
-        public static final int spielplan_spinner_label = 0x7f03000a;
-        public static final int start_activity_playing_schedule = 0x7f030003;
-        public static final int start_activity_ranking = 0x7f030002;
-        public static final int start_activity_set_login_data = 0x7f030001;
-        public static final int start_activity_teambet = 0x7f030004;
-        public static final int upload_bet_illegal_parameter = 0x7f030018;
-        public static final int upload_bet_late = 0x7f030016;
-        public static final int upload_bet_success = 0x7f030015;
-        public static final int upload_bet_unknown_user = 0x7f030017;
+        public static final int app_name = 0x7f040000;
+        public static final int default_get_server = 0x7f04000f;
+        public static final int default_post_server = 0x7f040010;
+        public static final int matchdetails_upload_bet = 0x7f040011;
+        public static final int rb_set_login_data_server_select_1 = 0x7f040013;
+        public static final int rb_set_login_data_server_select_2 = 0x7f040014;
+        public static final int server_location_menu_edit_server_data = 0x7f04000b;
+        public static final int set_login_data_choose_group = 0x7f040012;
+        public static final int set_login_data_name = 0x7f040005;
+        public static final int set_login_data_password = 0x7f040006;
+        public static final int set_login_data_save = 0x7f040008;
+        public static final int set_login_data_save_message = 0x7f040009;
+        public static final int set_login_data_show = 0x7f040007;
+        public static final int set_server_data_get_server = 0x7f04000c;
+        public static final int set_server_data_post_server = 0x7f04000d;
+        public static final int set_server_data_save = 0x7f04000e;
+        public static final int spielplan_spinner_label = 0x7f04000a;
+        public static final int start_activity_playing_schedule = 0x7f040003;
+        public static final int start_activity_ranking = 0x7f040002;
+        public static final int start_activity_set_login_data = 0x7f040001;
+        public static final int start_activity_teambet = 0x7f040004;
+        public static final int upload_bet_illegal_parameter = 0x7f040018;
+        public static final int upload_bet_late = 0x7f040016;
+        public static final int upload_bet_success = 0x7f040015;
+        public static final int upload_bet_unknown_user = 0x7f040017;
     }
 }
